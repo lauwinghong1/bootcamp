db383da35e96bf82421bd84847a45b747e9c6a2c
Lau Wing Hong <lauwinghong@Laus-MacBook-Air.local>
2024-11-14 22:08:58 +0800

    w6

--- Week6/src/Animal.java
+++ Week6/src/Animal.java
@@ -2,8 +2,9 @@ public class Animal {
   private int age;
 
   // implicity empty constructor
-  //public Animal(){
-  //}
+  public Animal(){
+  }
+  
   public Animal(int age){
     this.age=age;
   }
--- Week6/src/Cat.java
+++ Week6/src/Cat.java
@@ -1,4 +1,15 @@
 public class Cat extends Animal{
   // private int age;
   private String color;
+  
+  public Cat(int age, String incolor){
+    super(age); // call the parent constrcutor
+    this.color=incolor;
+  }
+
+  public static void main(String[] args) {
+    // empty constructor by default call the parent empty constructor
+    Cat c1 = new Cat(12, "Red");
+    System.out.println(c1.getAge());
+  } 
 }
